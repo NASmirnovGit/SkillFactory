7aa3815540071c0be4a4bfcf2046576d968a6a5d
NASmirnovGit <nikus1727@gmail.com>
2022-09-09 14:56:45 +0700

    Initial commit

--- 12.5.6.py
+++ 12.5.6.py
@@ -2,5 +2,8 @@ string = input("Введите числа через пробел:")
 
 list_of_strings = string.split() # список строковых представлений чисел
 list_of_numbers = list(map(int, list_of_strings)) # список чисел
+print(list_of_numbers)
+list_of_numbers[0], list_of_numbers[-1] = list_of_numbers [-1], list_of_numbers[0]
+print(list_of_numbers)
 
-print(sum(list_of_numbers[::3])) # sum() вычисляет сумму элементов списка
+print(sum(list_of_numbers[:])) # sum() вычисляет сумму элементов списка
