3134c6b355baa1bd70c9524b7ffb634f64f26011
NASmirnovGit <nikus1727@gmail.com>
2022-09-09 19:23:59 +0700

    Initial commit

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,8 +1,11 @@
 money = int(input('Введите величину депозита:'))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-# Создаем список значений процентной ставки в разных банках
-stavka = list(per_cent.values())
-# Создаем пустой список для внесения в него значений заработка
-deposit = [int(stavka[0] * money / 100), int(stavka[1] * money / 100), int(stavka[2] * money / 100),
-           int(stavka[3] * money / 100)]
-print(deposit, '\nМаксимальная сумма, которую вы можете заработать — ', max(deposit))
+# Создаем копию словаря
+deposit_dict = per_cent.copy()
+# Заменяем значения в словаре
+deposit_dict.update([('ТКБ', int(per_cent['ТКБ'] * money / 100)), ('СКБ', int(per_cent['СКБ'] * money / 100)),
+                     ('ВТБ', int(per_cent['ВТБ'] * money / 100)), ('СБЕР', int(per_cent['СБЕР'] * money / 100))])
+deposit_list = [int(per_cent['ТКБ'] * money / 100), int(per_cent['СКБ'] * money / 100),
+                int(per_cent['ВТБ'] * money / 100), int(per_cent['СБЕР'] * money / 100)]
+print('Вывод заработка в виде словаря:\n', deposit_dict,'\nВывод заработка в виде списка:\n',
+      deposit_list, '\nМаксимальная сумма, которую вы можете заработать — ', max(deposit_list))
